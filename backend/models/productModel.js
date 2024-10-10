dbee85c745c7da60edccf980defa44b03f993826
Raavan-monster <tushar1203yadav@gmail.com>
2024-10-10 16:13:02 +0530

    CRUD and Error

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -67,6 +67,4 @@ const productSchema = new mongoose.Schema({
 
 })
 
-//module.exports = mongoose.model("Product", productSchema)
-const Product = mongoose.model('Product', productSchema);
-module.exports = Product;
+module.exports = mongoose.model("Product", productSchema)
