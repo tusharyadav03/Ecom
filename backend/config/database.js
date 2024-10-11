da747b74350b1753e7ebe1daa7df9d44a7c26a97
Raavan-monster <tushar1203yadav@gmail.com>
2024-10-11 13:52:44 +0530

    All Error and ApiFeatures

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const connectDatabase = () => {
   mongoose.connect(process.env.DB_URI, { autoIndex: true }).then((data) => {
     console.log(`Mongodb connected with server: ${data.connection.host}`);
-  }).catch((err) => {
-    console.log(err)
   })
+
+  // .catch((err) => {
+  //   console.log(err)
+  // })
 }
 
 module.exports = connectDatabase
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const dotenv = require("dotenv");
 const connectDatabase = require("./config/database")
 
 
+//Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting Down the server due to Uncaught Exception`);
+  process.exit(1);
+})
+
 //Config
 
 dotenv.config({ path: "backend/config/config.env" });
@@ -11,6 +18,18 @@ dotenv.config({ path: "backend/config/config.env" });
 //Connectiong Database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`)
 })
+
+
+
+//Unhandled Promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log(`Shutting Down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
